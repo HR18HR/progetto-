4fff8284e469e6dded4e5e622573cb6e00b7a5da
Davide Lago <895388@stud.unive.it>
2025-11-21 13:17:18 +0100

    aggiunti models a backend

---
+++ back/models/User.ts
@@ -0,0 +1,11 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  email: { type: String, unique: true },
+  nickname: String,
+  salt: String,
+  digest: String,
+  saved_locations: [{ position: { lat: Number, lng: Number }, label: String }]
+});
+
+export default model("User", userSchema);
